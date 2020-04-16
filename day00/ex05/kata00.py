ed982b1045aa858a06697c468d836109a7d00ffc
Goretti Rivera <gorettirivera@Gorettis-MacBook-Pro.local>
2020-04-15 21:17:38 -0700

    day00

---
+++ day00/ex05/kata00.py
@@ -0,0 +1,5 @@
+
+t = (19, 42, 21)
+t_len = len(t)
+
+print(f"The {t_len} numbers are: {str(t)[1:-1]}")
---
+++ day00/ex05/kata01.py
@@ -0,0 +1,9 @@
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+
+for x,y in languages.items():
+    print(f'{x} was created by {y}')
+
